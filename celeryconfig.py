3d3797eb832a6e5ae04a8cf20ddcead6f572a14e
Chris Fuller <cfuller@brightcove.com>
2016-03-02 09:40:52 -0500

    removing environment-specific config file

--- celeryconfig.py
+++
@@ -1,8 +0,0 @@
-BROKER_URL = 'amqp://admin:Brightc0ve1@bc1.local/bc1.local'
-CELERY_RESULT_BACKEND = 'redis://127.0.0.1'
-#CELERY_TASK_SERIALIZER = 'json'
-#CELERY_RESULT_SERIALIZER = 'json'
-#CELERY_ACCEPT_CONTENT=['json']
-CELERY_TIMEZONE = 'US/Eastern'
-CELERY_ENABLE_UTC = True
-broker_api = 'http://admin:Brightc0ve1@bc1.local:15672/api/'
